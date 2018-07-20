5f79dd2967d233a828aee02d8333f1e7a057f5ee
kalistan <kaliesha.pickering@gmail.com>
2018-07-20 00:45:06 -0400

    html and start of JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+var $kenImage = $("#ken-image");
+var $sakuraImage =("#sakura-image");
+var $chibiImage = ("#chibi-image");
+var $ryuImage = ("#ryu-image");
+var $attackBtn = ("#attack-btn");
+var $playerHP
+var $computerHP
+var $playerXP
+var $computerXP
+
+var characters ={
+        "ken": {
+            name: "Ken",
+            hp: 100,
+            atk: 6,
+            counterAtk: 20
+        },
+        "sakura":{
+            name: "Sakura",
+            hp: 100,
+            atk: 6,
+            counterAtk: 10
+        },
+        "chibi":{
+            name: "Chibi",
+            hp: 100,
+            atk: 6,
+            counterAtk: 15
+        },
+        "ryu":{
+            name: "Ryu",
+            hp: 100,
+            atk: 6,
+            counterAtk: 25
+        }
+    }
+
+
+
+
+$(document).ready(function(){
+
+});
