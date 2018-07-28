52667e687da7bca912bcd064548e538874d60027
kalistan <kaliesha.pickering@gmail.com>
2018-07-27 22:03:49 -0400

    javascript logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,162 @@
-var $kenImage = $("#ken-image");
-var $sakuraImage =("#sakura-image");
-var $chibiImage = ("#chibi-image");
-var $ryuImage = ("#ryu-image");
-var $attackBtn = ("#attack-btn");
+var kenImage = $("#ken-image");
+var sakuraImage = $("#sakura-image");
+var chibiImage = $("#chibi-image");
+var ryuImage = $("#ryu-image");
+var attackBtn = $("#attack-btn");
 var $playerHP
 var $computerHP
 var $playerXP
 var $computerXP
+var $player1Div;
+var $player2Div;
 
 var game = {
-    player: "",
-    computerPlayer: "",
-    playerHP: 0,
-    computerHp: 0,
-    playerAtk: 0,
-    counterAtk:0,
+        player: "",
+        computerPlayer: "",
+        playerHP: 0,
+        computerHp: 0,
+        playerAtk: 0,
+        counterAtk: 0,
 
-    characters:{
-        "ken": {
-            name: "Ken",
-            hp: 100,
-            atk: 6,
-            counterAtk: 20
+        characters: {
+            "ken": {
+                name: "Ken",
+                hp: 125,
+                atk: 8,
+                counterAtk: 20
+            },
+            "sakura": {
+                name: "Sakura",
+                hp: 100,
+                atk: 4,
+                counterAtk: 10
+            },
+            "chibi": {
+                name: "Chibi",
+                hp: 115,
+                atk: 6,
+                counterAtk: 15
+            },
+            "ryu": {
+                name: "Ryu",
+                hp: 150,
+                atk: 10,
+                counterAtk: 25
+            }
         },
-        "sakura":{
-            name: "Sakura",
-            hp: 100,
-            atk: 6,
-            counterAtk: 10
+
+        //select player
+        selectCharacter: function (player) {
+            if (this.player === " ") {
+                this.player = player;
+                this.playerHp = this.character[this.player].hp;
+                this.playerAtk = this.character[this.player].atk;
+                var attacker = $("<.attacker>");
+                attacker.addClass(this.character[player]);
+                attacker.attr();
+                atttacker.appendTo(".attacker")
+                for (var opponent in this.characters) {
+                    if (opponent !== this.player) {
+                        this.computerPlayer = opponent;
+                    }
+                }
+                var opponent = $(".opponent");
+                opponent.appendTo(this.computerPlayer);
+                this.computerHP = this.character[this.computerPlayer].hp;
+                this, counterAtk = this.character[this.computerPlayer].counterAtk;
+            }
+            this.startGame();
         },
-        "chibi":{
-            name: "Chibi",
-            hp: 100,
-            atk: 6,
-            counterAtk: 15
+
+        //set-up of game
+        setUp: function () {
+
+            $player1Div = $("<div>");
+            $player2Div = $("<div>");
+            $playerHP = $('<div role="progressbar" aria-valuemin="0" style="width: 0%;">');
+            $computerHP = $('<div role="progressbar"  aria-valuemin="0" style="width: 0%;">');
+            $playerXP = $('<div role="progressbar" aria-valuemin="0" style="width: 0%;">');
+            $computerXP = $('<div role="progressbar"  aria-valuemin="0" style="width: 0%;">');
+            $player1Div.addClass('progress');
+            $player2Div.addClass('progress');
+            $playerBar.addClass('progress-bar');
+            $opponentBar.addClass('progress-bar progress-bar-danger');
+            $playerHealth.css('width', this.playerHP + '%');
+            $opponentHealth.css('width', this.computerHP + '%');
+            $playerXP.css('width', this.playerXP + '%');
+            $opponentXP.css('width', this.computerXP + '%');
+            $('.player').append($player1Div.append($playerBar));
+            $('.opponent').append($player2Div.append($opponentBar));
+
         },
-        "ryu":{
-            name: "Ryu",
-            hp: 100,
-            atk: 6,
-            counterAtk: 25
-        }},
 
-           //select player
-selectCharacter: function(player){
-   if (this.player === " "){
-    this.player = player;
-    this.playerHp = this.character[this.player].hp;
-    this.playerAtk = this.character[this.player].atk;
-    var attacker =  $("<attacker>");
-    attacker.addClass(this.character[player]);
-    attacker.attr();
-    atttacker.appendTo(".attacker")
-for(var opponent in this.characters){
-    if(opponent !== this.player){
-        this.computerPlayer = opponent;
-    }
-}
-this.computerHP = this.character[this.computerPlayer].atk;
-this,counterAtk = this.character[this.computerPlayer].counterAtk;
-   }
-///start game
-this.startGame();
-},
+        startGame: function () {
+            this.setUp();
+            $attackBtn.addClass('btn btn-danger btn-lg');
+            $attackBtn.html("Attack!");
+            $("#attack-btn").append($attackBtn);
+        },
+        //attack function
+        fight: function () {
+                        var randomAtkMultiplier = Math.floor(Math.random() * 4) + 1;
+                        $playerStats.hide().html(this.player + " did an attack of " + this.playerAttack * randomAtkMultiplier);
+                        $computerStats.hide().html(this.computerPlayer + " did a counter attack of " + this.computerAttack);
+                    },
 
-startGame: function(){
+                    //update players
+                    update: function () {
+                        $opponentBar.css('width', this.computerHP + '%');
+                        this.playerHp -= (this.computerAttack);
+                        $playerBar.html(this.playerHP + '%');
+                        $opponentBar.html(this.computerHp + '%');
+                        $playerBar.css('width', this.playerHP + '%');
+                        $playerBar.html(this.playerXP + '%');
+                        $playerBar.css('width', this.playerXP + '%');
 
-},
-//attack function
-fight: function(){
-    var playerAttack ={
-        attack : function(){
+                        this.checkForWinner();
+                    },
 
-        }
-       // for(loop) to increase attack strength
+                    //check for winner
+                    checkForWinner: function () {
+                        if (this.computerrHp <= 0) {
+                            //you win
+                            alert('You Win!');
 
-        //decrease opponet health
+                        } else if (this.playerHp <= 0) {
+                            //you lose
+                            alert('You Lose!');
+                        }
+                    }
 
-        //counter atk decrease player health
-        
-    }
-}
-
-}
- 
-    
+                }
 
 
 
-$(document).ready(function(){
+                // playing the actual game
 
-});
+                $(document).ready(function () {
+
+                    kenImage.on('click', function () {
+                        var data = $(this).data("name");
+                        game.selectPlayer(data);
+                    });
+
+                    ryuImage.on('click', function () {
+                        var data = $(this).data("name");
+                        game.selectPlayer(data);
+                    });
+
+                    sakuraImage.on('click', function () {
+                        var data = $(this).data("name");
+                        game.selectPlayer(data)
+                    });
+                    chibiImage.on('click', function () {
+                        var data = $(this).data("name");
+                        game.selectPlayer(data)
+                    });
+
+                    attackBtn.on('click', function () {
+                        game.fight();
+                        game.update();
+                    });
+                });
